c22e0ce9fdce36e1bb5540c884d1a9d425bd32e8
Elhadji <embaye@aimsammi.org>
2025-04-12 19:51:57 +0000

    ff

---
+++ data_structures/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="data-structures-package",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A collection of data structure implementations",
+    author="Elhadji Mamadou",
+    author_email="your.email@example.com",
+    python_requires=">=3.6",
+)
