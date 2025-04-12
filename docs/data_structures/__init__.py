05a64c266d15f7a04c47b5a45323ed8f3135be9c
mamad9633 <92327014+mamad9633@users.noreply.github.com>
2025-04-12 20:12:02 +0000

    Delete docs directory

--- docs/data_structures/__init__.py
+++
@@ -1,7 +0,0 @@
-from .array import Array
-from .linkedlist import LinkedList
-from .queue import Queue
-from .stack import Stack
-from .tree import BinarySearchTree
-
-__all__ = ['Array', 'LinkedList', 'Queue', 'Stack', 'BinarySearchTree']
